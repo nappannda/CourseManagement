76d72dc97a9d47c9dc8daf60f32fab70797469ac
nappannda <qwe159753qwee@gmail.com>
2017-12-22 18:51:37 +0900

    Fix Status Enum ==== # 🐛  :bug: バグ修正 # 👍  :+1: 機能改善 # ✨  :sparkles: 部分的な機能追加 # 🎉  :tada: 盛大に祝うべき大きな機能追加 # ♻️  :recycle: リファクタリング # 🚿  :shower: 不要な機能・使われなくなった機能の削除 # 💚  :green_heart: テストやCIの修正・改善 # 👕  :shirt: Lintエラーの修正やコードスタイルの修正 # 🚀  :rocket: パフォーマンス改善 # 🆙  :up: 依存パッケージなどのアップデート # 🔒  :lock: 新機能の公開範囲の制限 # 👮  :cop: セキュリティ関連の改善
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- app/src/main/java/develop/nappa/coursemanagement/model/Course.kt
+++ app/src/main/java/develop/nappa/coursemanagement/model/Course.kt
@@ -17,28 +17,19 @@ open class Course : RealmObject() {
     open var createdAt : Date = Date()
     open var updatedAt : Date = Date()
 
-    enum class Status(val integerValue: Int) {
-        NONE(1), // なし
-        DOING(2), // 履修中
-        ENROLLED(3), // 履修済み
-        RETAKE(4) // 再履修
+    enum class Status(val stringValue: String) {
+        NONE("なし"),
+        DOING("履修中"),
+        ENROLLED("履修済み"),
+        RETAKE("再履修")
     }
 
-    private var statusValue: Int = Status.NONE.integerValue
+    private var statusValue: String = Status.NONE.stringValue
     open var status: Status
-        get() = Status.values().first { it.integerValue == statusValue }
+        get() = Status.values().first { it.stringValue == statusValue }
         set(value) {
-            statusValue = value.integerValue
+            statusValue = value.stringValue
         }
-
-    fun statusString(): String {
-        return when (status) {
-            Status.NONE -> "なし"
-            Status.DOING -> "履修中"
-            Status.ENROLLED -> "履修済み"
-            Status.RETAKE -> "再履修"
-        }
-    }
 
     fun initialize() {
         name = ""
