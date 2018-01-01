5abbc6ea501d43f244dd9beaf581c67f3087effd
nappannda <qwe159753qwee@gmail.com>
2018-01-02 00:34:21 +0900

    Apply Changed Property Model
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # ♻️  :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- app/src/main/java/develop/nappa/coursemanagement/activity/CourseDetailActivity.kt
+++ app/src/main/java/develop/nappa/coursemanagement/activity/CourseDetailActivity.kt
@@ -3,6 +3,7 @@ package develop.nappa.coursemanagement.activity
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import develop.nappa.coursemanagement.R
 import develop.nappa.coursemanagement.databinding.ActivityCourseDetailBinding
 import develop.nappa.coursemanagement.model.Course
@@ -13,22 +14,71 @@ import io.realm.Realm
  */
 class CourseDetailActivity : AppCompatActivity() {
 
-    private var realm: Realm? = null
+    private lateinit var realm: Realm
+    private lateinit var binding: ActivityCourseDetailBinding
+
+    private var course: Course? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         realm = Realm.getDefaultInstance()
 
-        val binding : ActivityCourseDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_course_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_course_detail)
+        binding.courseDetail = this
+
         val id = intent.getStringExtra("id")
-        val course: Course =  realm!!.where(Course::class.java).contains("id", id).findFirst()
-        supportActionBar?.title = course.name
+        course =  realm.where(Course::class.java).contains("id", id).findFirst()
+        supportActionBar?.title = course?.name
         binding.course = course
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        realm?.close()
+        realm.close()
+    }
+
+    fun courseNameTextChanged(text: CharSequence) {
+        realm.beginTransaction()
+        course?.name = text.toString()
+        realm.commitTransaction()
+    }
+
+    fun unitTextChanged(text: CharSequence) {
+        var unit = 0
+        try {
+            unit = text.toString().toInt()
+        }
+        catch (e: NumberFormatException) {
+            Toast.makeText(this, "数字を入力してください", Toast.LENGTH_SHORT).show()
+            binding.unitEditText.setText("0")
+        }
+        realm.beginTransaction()
+        course?.unit = unit
+        realm.commitTransaction()
+    }
+
+    fun memoTextChanged(text: CharSequence) {
+        realm.beginTransaction()
+        course?.memo = text.toString()
+        realm.commitTransaction()
+    }
+
+    fun plusAttendance() {
+        realm.beginTransaction()
+        course!!.attendanceCount = course!!.attendanceCount.inc()
+        realm.commitTransaction()
+        binding.attendanceCountNumberText.setText(course!!.attendanceCount.toString())
+    }
+
+    fun minusAttendance() {
+        if (course!!.attendanceCount == 0) {
+            Toast.makeText(this, "出席数を0未満に設定することは出来ません", Toast.LENGTH_SHORT).show()
+            return
+        }
+        realm.beginTransaction()
+        course!!.attendanceCount = course!!.attendanceCount.dec()
+        realm.commitTransaction()
+        binding.attendanceCountNumberText.setText(course!!.attendanceCount.toString())
     }
 }
